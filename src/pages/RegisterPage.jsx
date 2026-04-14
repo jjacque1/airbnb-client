dae33f3940a756dc30b7fe018cdafbd882ed7569
jjacque1 <jacksonjacque.jj@gmail.com>
2026-04-14 11:28:03 -0400

    Register Form structure created with controlled inputs

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,52 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+  }
+
   return (
-    <div>
-      <h1> RegisterPage </h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <header>
+        <h1>Register</h1>
+      </header>
+      <div>
+        <label htmlFor="fullName">Full Name: </label>
+        <input
+          id="fullName"
+          type="text"
+          value={fullName}
+          onChange={(event) => {
+            setFullName(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email: </label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
