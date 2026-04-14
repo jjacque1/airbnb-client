69e164afcd0354730e777e3d99c42ccf7f7b2944
jjacque1 <jacksonjacque.jj@gmail.com>
2026-04-14 10:57:32 -0400

    remove .env from git tracking, removing temp useEffect from homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,5 @@
-import api from "../api/api"
-import { useEffect } from "react"
-
 export default function HomePage() {
-    useEffect(() => {
-        const fetchHealth = async () => {
-            try {
-                const response = await api.get("/health");
-                console.log("API response:", response.data);
-            } catch (error) {
-                console.error("API error:", error)
-            }
-        };
-
-        fetchHealth();
-    }, []);
+    
     
     return (
         <div>
