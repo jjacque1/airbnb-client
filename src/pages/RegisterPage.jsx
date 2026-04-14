89a9cff14ceea0d31a79c352539fbdc325f5845c
jjacque1 <jacksonjacque.jj@gmail.com>
2026-04-14 12:27:37 -0400

    implement register form with API intergration

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+import api from "../api/api";
 
 export default function RegisterPage() {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    
+    try {
+      const response = await api.post("/auth/register", {
+        fullName,
+        email,
+        password,
+      });
+
+      console.log("Register success:", response.data);
+
+      setFullName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+    }
   }
 
   return (
